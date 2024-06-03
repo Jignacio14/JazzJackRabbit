d25e9343590cdc7e05c56bf307868322e4eb0b67
Jignacio14 <j14castro2001@gmail.com>
2024-06-02 23:09:11 -0300

    Agrego constantes y referencias para que no rompa el analisis en github

--- src/data/player_info_dto.h
+++ src/data/player_info_dto.h
@@ -8,8 +8,8 @@ struct PlayerInfo {
   std::vector<char> player_name;
   std::vector<char> game_name;
   uint8_t character_code;
-  PlayerInfo(std::vector<char> player_name, std::vector<char> game_name,
-             uint8_t character_code)
+  PlayerInfo(const std::vector<char> &player_name,
+             const std::vector<char> &game_name, const uint8_t &character_code)
       : player_name(player_name), game_name(game_name),
         character_code(character_code) {}
   PlayerInfo() : player_name(), game_name(), character_code(0) {}
