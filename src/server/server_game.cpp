3cdf08ce01b9bf5c9c1ab25c3361fc716166340a
Jignacio14 <j14castro2001@gmail.com>
2024-06-03 21:40:38 -0300

    Agrego base del juego

--- src/server/server_game.cpp
+++ src/server/server_game.cpp
@@ -1,5 +1,36 @@
 #include "server_game.h"
+#include <iostream>
+#include <thread>
+#define MIN_PLAYERS 2
 
-Game::Game(GameMonitor &monitor) : monitor(monitor) {}
+Game::Game(GameMonitor &monitor, Queue<BaseDTO *> &queue)
+    : monitor(monitor), messages(queue), players(0) {}
+
+void Game::waitingRoomLoop() {
+  while (this->players < MIN_PLAYERS) {
+  }
+}
+
+void Game::gameLoop() {
+  while (this->_is_alive) {
+    /// Empiezo a calcular la diferencia de tiempo para hacer el sleep
+    BaseDTO *dto;
+    if (!this->messages.try_pop(dto)) {
+      continue;
+    }
+    this->executeAction(dto);
+    delete dto;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+}
+
+void Game::executeAction(BaseDTO *dto) {
+  std::cout << "Executing action" << std::endl;
+}
+
+void Game::run() {
+  this->waitingRoomLoop();
+  this->gameLoop();
+}
 
 void Game::kill() { this->_is_alive = false; }
--- src/server/server_game.h
+++ src/server/server_game.h
@@ -3,16 +3,24 @@
 
 #include "../common/thread.h"
 #include "server_game_monitor.h"
+#include <atomic>
 #include <list>
 #include <map>
 
 class Game : public Thread {
 private:
   GameMonitor &monitor;
+  Queue<BaseDTO *> &messages;
+  std::atomic_int32_t players;
+  void gameLoop();
+  void waitingRoomLoop();
+  void executeAction(BaseDTO *dto);
 
 public:
-  explicit Game(GameMonitor &monitor);
+  explicit Game(GameMonitor &monitor, Queue<BaseDTO *> &messages);
   void kill();
+  void run() override;
+  void addPlayer(const PlayerInfo &player_info);
 };
 
 #endif
