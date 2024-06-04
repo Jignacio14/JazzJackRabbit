62c9aa857f31663ae297537f94d91fb25b1828f0
gabrieldiem <gabriel.diem5@gmail.com>
2024-06-04 14:38:56 -0300

    feat: Added new variables to config yaml file

--- src/common/global_configs.cpp
+++ src/common/global_configs.cpp
@@ -14,9 +14,28 @@ GlobalConfigs::GlobalConfigs() {
 
   this->maxPlayersPerGame = gameConfigs["max_players_per_game"].as<uint16_t>();
   this->maxGameDuration = gameConfigs["max_game_duration"].as<uint32_t>();
+  this->maxUsernameLength = gameConfigs["max_username_length"].as<uint32_t>();
+  this->minNumberOfPlayers =
+      gameConfigs["min_number_of_players"].as<uint32_t>();
+  this->maxPortNumber = gameConfigs["max_port_number"].as<uint32_t>();
+
+  this->screenSizeX = gameConfigs["screen_size_x"].as<int>();
+  this->screenSizeY = gameConfigs["screen_size_y"].as<int>();
 }
 
 uint16_t GlobalConfigs::getMaxPlayersPerGame() {
   return this->maxPlayersPerGame;
 }
 uint32_t GlobalConfigs::getMaxGameDuration() { return this->maxGameDuration; }
+
+uint32_t GlobalConfigs::getMaxUsernameLength() {
+  return this->maxUsernameLength;
+}
+uint32_t GlobalConfigs::getMinNumberOfPlayers() {
+  return this->minNumberOfPlayers;
+}
+uint32_t GlobalConfigs::getMaxPortNumber() { return this->maxPortNumber; }
+
+int GlobalConfigs::getScreenSizeX() { return this->screenSizeX; }
+
+int GlobalConfigs::getScreenSizeY() { return this->screenSizeY; }
--- src/common/global_configs.h
+++ src/common/global_configs.h
@@ -9,6 +9,16 @@ private:
   uint16_t maxPlayersPerGame;
   // cppcheck-suppress unusedStructMember
   uint32_t maxGameDuration;
+  // cppcheck-suppress unusedStructMember
+  uint32_t maxUsernameLength;
+  // cppcheck-suppress unusedStructMember
+  uint32_t minNumberOfPlayers;
+  // cppcheck-suppress unusedStructMember
+  uint32_t maxPortNumber;
+  // cppcheck-suppress unusedStructMember
+  int screenSizeX;
+  // cppcheck-suppress unusedStructMember
+  int screenSizeY;
 
   GlobalConfigs();
 
@@ -33,6 +43,11 @@ public:
 
   uint16_t getMaxPlayersPerGame();
   uint32_t getMaxGameDuration();
+  uint32_t getMaxUsernameLength();
+  uint32_t getMinNumberOfPlayers();
+  uint32_t getMaxPortNumber();
+  int getScreenSizeX();
+  int getScreenSizeY();
 };
 
 #endif // GLOBAL_CONFIGS_H
