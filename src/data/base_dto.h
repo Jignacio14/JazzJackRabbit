dddb4378c396d15c6184e733827e3fb195284d17
Jignacio14 <j14castro2001@gmail.com>
2024-06-03 23:34:47 -0300

    Agrego comportamiento al dto base

--- src/data/base_dto.h
+++ src/data/base_dto.h
@@ -1,9 +1,20 @@
 #ifndef BASE_DTO
 #define BASE_DTO
 
+#include <cstdint>
 class BaseDTO {
+private:
+  // cppcheck-suppress unusedStructMember
+  uint8_t operation_id;
+  // cppcheck-suppress unusedStructMember
+  uint8_t player_id;
+
 public:
-  BaseDTO();
+  explicit BaseDTO(uint8_t operation_id, uint8_t player_id);
+
+  uint8_t getOperationId() const;
+  uint8_t getPlayerId() const;
+
   ~BaseDTO();
 };
 
