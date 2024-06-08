ab6d7cc99a0b9aba0de14865e16935b89ad9e8b7
Jignacio14 <j14castro2001@gmail.com>
2024-06-08 00:15:33 -0300

    Elimino base dto del archivo server_game_monitor.h

--- src/server/server_game_monitor.cpp
+++ src/server/server_game_monitor.cpp
@@ -1,30 +1,29 @@
 #include "./server_game_monitor.h"
-#include <mutex>
-#include <sys/types.h>
 
 GameMonitor::GameMonitor() : players_count(0) {}
 
-void GameMonitor::broadcast(BaseDTO *&msj) {
+void GameMonitor::broadcast(Snapshot &msj) {
   std::lock_guard<std::mutex> lock(mtx);
-  for (auto iter = this->queues.begin(); iter != this->queues.end(); iter++) {
-    Queue<BaseDTO *> &queue = iter->get();
-    try {
-      queue.push(msj);
-    } catch (const ClosedQueue &) {
-      iter = queues.erase(iter);
-    }
-  }
+  // for (auto iter = this->queues.begin(); iter != this->queues.end(), iter++)
+  // {
+  //   Queue<BaseDTO *>& queue = iter->get();
+  //   try {
+  //     queue.push(&msj);
+  //   } catch (const ClosedQueue &) {
+  //     iter = queues.erase(iter);
+  //   }
+  // }
 }
 
 const u_int8_t GameMonitor::addPlayer(PlayerInfo &player_info,
-                                      Queue<BaseDTO *> &client) {
+                                      Queue<Snapshot &> &client) {
   std::lock_guard<std::mutex> lck(mtx);
   queues.push_back(client);
   players_count++;
   return players_count;
 }
 
-void GameMonitor::ereasePlayer(const Queue<BaseDTO *> &client) {
+void GameMonitor::ereasePlayer(const Queue<Snapshot &> &client) {
   std::lock_guard<std::mutex> lck(mtx);
   queues.remove_if(
       [&client](const std::reference_wrapper<Queue<BaseDTO *>> &queue_ref) {
