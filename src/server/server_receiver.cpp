077a4229fcb5e99fbf9a925e64e6f6bd432e152a
Jignacio14 <j14castro2001@gmail.com>
2024-06-08 00:35:48 -0300

    Elimino base_dto del receiver

--- src/server/server_receiver.cpp
+++ src/server/server_receiver.cpp
@@ -1,18 +1,14 @@
 #include "./server_receiver.h"
 #include "./server_protocol.h"
 
-Receiver::Receiver(ServerProtocol &servprot, Queue<BaseDTO *> &receiver_queue)
+Receiver::Receiver(ServerProtocol &servprot, Queue<Snapshot> &receiver_queue)
     : servprot(servprot), receiver_queue(receiver_queue) {}
 
 void Receiver::run() {
   while (this->is_alive()) {
-    BaseDTO *dto;
     // BaseDTO *dto = this->servprot.receive(dto);
     // cppcheck-suppress uninitvar
-    if (dto == nullptr) {
-      continue;
-    }
-    this->receiver_queue.try_pop(dto);
+    // this->receiver_queue.try_pop(dto);
   }
 }
 
--- src/server/server_receiver.h
+++ src/server/server_receiver.h
@@ -2,17 +2,17 @@
 #define RECEIVER
 
 #include "../common/queue.h"
+#include "../common/snapshot_DTO.h"
 #include "../common/thread.h"
-#include "../data/base_dto.h"
 #include "./server_protocol.h"
 
 class Receiver : public Thread {
 private:
   ServerProtocol &servprot;
-  Queue<BaseDTO *> &receiver_queue;
+  Queue<Snapshot> &receiver_queue;
 
 public:
-  explicit Receiver(ServerProtocol &servprot, Queue<BaseDTO *> &receiver_queue);
+  explicit Receiver(ServerProtocol &servprot, Queue<Snapshot> &receiver_queue);
   void run() override;
   void kill();
   ~Receiver() override;
