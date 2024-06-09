6b8113c57100e61898bf2c3d07fea7c02630b39d
gabrieldiem <gabriel.diem5@gmail.com>
2024-06-09 12:16:14 -0300

    feat: Added Enums for Snapshot dto conventions. Added SnapshotWrapper to make the struct 'smart'. Added initial values in yaml config file

---
+++ src/data/convention.h
@@ -0,0 +1,23 @@
+
+enum class PlayableCharactersIds {
+  Jazz = 0,
+  Spaz = 1,
+  Lori = 2,
+  NoneSelected = 9
+};
+
+enum class GunsIds { Gun1 = 1, Gun2 = 2 };
+
+enum class NumericBool { True = 0, False = 1 };
+
+enum class FacingDirectionsIds { Left = 0, Right = 1, Up = 2, Down = 3 };
+
+enum class CollectableIds {
+  Coin = 0,
+  Diamond = 1,
+  Carrot = 2,
+  AmmoGun1 = 3,
+  AmmoGun2 = 4
+};
+
+enum class EnemiesIds { Bubba = 0, TurtleGoon = 1, Schwarzenguard = 2 };
