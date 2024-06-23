b0ec1f90c13825b56e0dc56161cea88d633b05fa
Jignacio14 <j14castro2001@gmail.com>
2024-06-23 00:16:30 -0300

    Agrego archivo de pruebas

---
+++ test/JazzJackRabbitTest.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+TEST(simple_test_case, sample_test) { EXPECT_EQ(1, 1); }
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
