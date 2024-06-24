0de0c25724dfeb78ac52f8d698e36b773ca64980
Jignacio14 <j14castro2001@gmail.com>
2024-06-24 00:58:45 -0300

    Agrego modificaciones sobre el test

--- test/testProtocoloServidor.cpp
+++ test/testProtocoloServidor.cpp
@@ -1,5 +1,6 @@
 #include "../src/common/socket.h"
 #include "../src/server/server_protocol.h"
+#include <iostream>
 #include <sys/types.h>
 
 int main() {
@@ -8,8 +9,10 @@ int main() {
     Socket peer = socket.accept();
     ServerProtocol protocol(std::move(peer));
     uint8_t id = protocol.getLobbyOption();
-    bool result = id == 1;
-    std::cout << result << std::endl;
+    bool result = (id == 1);
+    if (result) {
+      std::cout << "Test passed" << std::endl;
+    }
     socket.shutdown(2);
     std::cout << "Test passed" << std::endl;
   } catch (...) {
